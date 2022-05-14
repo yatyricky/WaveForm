e2df16c4ae4c6d919a657d424a79cfc24fa93c7d
Rick Sun <yatyricky@gmail.com>
2022-05-14 12:30:53 +0800

    shader1

--- Assets/Scripts/BeatDetection.cs
+++ Assets/Scripts/BeatDetection.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,15 +8,20 @@ public class BeatDetection : MonoBehaviour
 {
     public int blockSize = 1024;
 
-    private float[][] _samples;
+    [Range(1, 64)]
+    public int prevFrames = 10;
 
-    public float[] max;
-    public float[] min;
+    private float[][] _samples;
+    private float[][][] _prevSamples;
+
+    // public float[] max;
+    // public float[] min;
 
     public int channelsCount = 2;
 
     public Transform[] channels;
     public Material[] materials;
+    private Material _imageMat;
 
     public int sampleRate;
 
@@ -29,6 +34,7 @@ public class BeatDetection : MonoBehaviour
     private int _avgEIndex;
     private float _varE;
     private float _c;
+    private float _maxC;
 
     public Transform averageE;
     public Transform c;
@@ -39,12 +45,18 @@ public class BeatDetection : MonoBehaviour
     private Image _imgRawSample;
     private Color[] _pixelsRawSample;
 
-    private static Color _white = Color.white;
-    private static Color _black = Color.black;
+    private static Color _white = new Color(1, 1, 1, 1);
+    private static Color _black = new Color(0, 0, 0, 0);
+    private static readonly int BeatStrength = Shader.PropertyToID("_BeatStrength");
 
     private static float WrapSample(float sample)
     {
         return (sample + 1f) / 2f;
+    }
+
+    private static float Divide(float dividend, float divisor)
+    {
+        return Mathf.Approximately(divisor, 0.000001f) ? 0f : dividend / divisor;
     }
 
     public void ComputeSoundEnergy()
@@ -78,45 +90,60 @@ public class BeatDetection : MonoBehaviour
         var currAvgEIndex = _avgEIndex++ % _sampleBlocks;
         frameAvgE[currAvgEIndex] = sum;
 
-        // var errorSum = 0f;
-        // for (int i = 0; i < _sampleBlocks; i++)
-        // {
-        //     var error = Mathf.Pow(sum - _frameEnergy[(_frameIndex + i) % _sampleBlocks], 2f);
-        //     errorSum += error;
-        // }
+        var errorSum = 0f;
+        for (int i = 0; i < _sampleBlocks; i++)
+        {
+            var error = Mathf.Pow(sum - frameEnergy[(_frameIndex + i) % _sampleBlocks], 2f);
+            errorSum += error;
+        }
 
-        // errorSum /= _sampleBlocks;
-        // _varE = errorSum;
-        // _c = mult * _varE + 1.5142857f;
+        errorSum /= _sampleBlocks;
+        _varE = errorSum;
+        _c = Mathf.Abs(mult * _varE + 1.5142857f);
+        _maxC = Mathf.Max(_c, _maxC);
 
         var height = frameAvgE[currAvgEIndex];
         averageE.localScale = new Vector3(10f, height, 1f);
         var cpos = averageE.localPosition;
         averageE.localPosition = new Vector3(cpos.x, height / 2f, cpos.z);
 
-        // height = _c * 3f;
-        // c.localScale = new Vector3(10f, height, 1f);
-        // cpos = c.localPosition;
-        // c.localPosition = new Vector3(cpos.x, height / 2f, cpos.z);
+        height = Divide(_c, _maxC) * 100;
+        c.localScale = new Vector3(10f, height, 1f);
+        cpos = c.localPosition;
+        c.localPosition = new Vector3(cpos.x, height / 2f, cpos.z);
+        
+        _imageMat.SetFloat(BeatStrength, _c);
     }
 
     // Start is called before the first frame update
     private void Start()
     {
-        _sampleBlocks = Mathf.FloorToInt(sampleRate / blockSize);
+        prevFrames = Mathf.FloorToInt(Mathf.Pow(2, Mathf.FloorToInt(Mathf.Log(prevFrames) / Mathf.Log(2))));
+        _sampleBlocks = Mathf.FloorToInt((float) sampleRate / blockSize);
         frameEnergy = new float[_sampleBlocks];
         frameAvgE = new float[_sampleBlocks];
         _avgEIndex = 0;
 
         _samples = new float[channelsCount][];
-        max = new float[channelsCount];
-        min = new float[channelsCount];
+        _prevSamples = new float[prevFrames][][];
+        for (int i = 0; i < prevFrames; i++)
+        {
+            _prevSamples[i] = new float[channelsCount][];
+        }
+
+        // max = new float[channelsCount];
+        // min = new float[channelsCount];
         _audioSource = GetComponent<AudioSource>();
         for (var chn = 0; chn < channelsCount; chn++)
         {
             _samples[chn] = new float[blockSize];
-            max[chn] = -99999999f;
-            min[chn] = 99999999f;
+            for (int i = 0; i < prevFrames; i++)
+            {
+                _prevSamples[i][chn] = new float[blockSize];
+            }
+
+            // max[chn] = -99999999f;
+            // min[chn] = 99999999f;
             // for (var i = 0; i < blockSize; i++)
             // {
             //     var obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
@@ -128,50 +155,62 @@ public class BeatDetection : MonoBehaviour
         _imgRawSample = transform.Find("Canvas/sp_raw_sample").GetComponent<Image>();
         _rawSample = _imgRawSample.sprite.texture;
         _pixelsRawSample = new Color[1024 * 1024];
+        _imageMat = _imgRawSample.material;
+    }
+
+    private static int Normalize(float channel0, float channel1)
+    {
+        var t1024_0 = Mathf.FloorToInt((channel0 + 1f) / 2f * 1024);
+        var t1024_1 = Mathf.FloorToInt((channel1 + 1f) / 2f * 1024);
+        return (t1024_0 + t1024_1) / 2;
+    }
+
+    private static int ModIndex(int i, int offset, int mod)
+    {
+        return (i + offset + mod) % mod;
     }
 
     private void Update()
     {
+        var currentFrameOfPrevFrames = Time.frameCount % prevFrames;
+        // for (int i = 0; i < channelsCount; i++)
+        // {
+        //     Array.Copy(_samples[i], _prevSamples[currentFrameOfPrevFrames][i], _samples[i].Length);
+        // }
+
         for (var chn = 0; chn < channelsCount; chn++)
         {
             _audioSource.GetOutputData(_samples[chn], chn);
-            max[chn] = Mathf.Max(_samples[chn].Max(), max[chn]);
-            min[chn] = Mathf.Min(_samples[chn].Min(), min[chn]);
-            for (var i = 0; i < blockSize; i++)
+            Array.Copy(_samples[chn], _prevSamples[currentFrameOfPrevFrames][chn], _samples[chn].Length);
+            // max[chn] = Mathf.Max(_samples[chn].Max(), max[chn]);
+            // min[chn] = Mathf.Min(_samples[chn].Min(), min[chn]);
+        }
+
+        var i = 0;
+        for (var y = 0; y < prevFrames; y++)
+        {
+            var x = (y + currentFrameOfPrevFrames) % prevFrames;
+            for (var n = 0; n < blockSize; n += prevFrames)
             {
-                var rawS = _samples[chn][i];
-                // var s = WrapSample(rawS);
-                // var x = (float) i; // i % 32;
-                // var y = 0f; //i / 32;
-                // var obj = channels[chn].GetChild(i);
-                // var h = s * 500f;
-                // obj.localScale = new Vector3(1, h, 1);
-                // obj.localPosition = new Vector3(x, h / 2, y);
+                var curr = Normalize(_prevSamples[x][0][n], _prevSamples[x][1][n]);
 
                 // norm 2 1024
-                if (chn == 1)
+                for (var j = 0; j < curr; j++)
                 {
-                    var t1024_0 = Mathf.FloorToInt((rawS + 1f) / 2f * 1024);
-                    var t1024_1 = Mathf.FloorToInt((_samples[0][i] + 1f) / 2f * 1024);
-                    var t1024 = (t1024_0 + t1024_1) / 2;
-                    for (int j = 0; j < t1024; j++)
-                    {
-                        _pixelsRawSample[i + j * 1024] = _black;
-                    }
-
-                    for (int j = t1024; j < 1024; j++)
-                    {
-                        _pixelsRawSample[i + j * 1024] = _white;
-                    }
+                    _pixelsRawSample[i + j * 1024] = _black;
                 }
-            }
 
-            if (chn == 1)
-            {
-                _rawSample.SetPixels(0, 0, 1024, 1024, _pixelsRawSample);
-                _rawSample.Apply();
+                for (var j = curr; j < 1024; j++)
+                {
+                    _pixelsRawSample[i + j * 1024] = _white;
+                }
+
+                i++;
             }
         }
+
+        _rawSample.SetPixels(0, 0, 1024, 1024, _pixelsRawSample);
+        _rawSample.Apply();
 
         ComputeSoundEnergy();
         UpdateAverageE();
